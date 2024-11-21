401233a6bf00b6c0149b5e6fe72849c1e86cc6d5
AngelMaga23 <angelmaga23@outlook.com>
2024-11-21 12:51:57 -0600

    Add Config DB Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from './config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host: envs.dbHost,
+      port: envs.dbPort,
+      database: envs.dbName,
+      username: envs.dbUsername,
+      password: envs.dbPassword,
+      autoLoadEntities: true,
+      synchronize:false
+    }),
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
